74148043c09e69c63fc7a2e2cfd9c2ec851cea43
Reactified <54720381+Reactified@users.noreply.github.com>
2019-09-03 15:19:03 -0400

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -1,7 +1,16 @@
 --/ Redhack Installer / Reactified /--
 local repo = "http://raw.github.com/Reactified/redhack/master/"
+local files = {
+    {"startup.lua","startup.lua"},
+    {"config.sys","sys/config.sys"},
+}
 
 --/ Function /--
+local w,h = term.getSize()
+local accent = colors.blue
+if term.isColor() then
+    accent = colors.black
+end
 local function getfile(gitfile,target)
     h = http.get(repo..gitfile)
     if h then
@@ -13,5 +22,17 @@ local function getfile(gitfile,target)
         return false
     end
 end
+local function drawTab(tab)
+    term.setBackgroundColor(colors.gray)
+    term.clear()
+    term.setCursorPos(1,1)
+    term.setBackgroundColor(accent)
+    term.clearLine()
+    term.setCursorPos(2,1)
+    term.setTextColor(colors.white)
+    term.write("Redhack Installer")
+    term.setCursorPos(w-#tab,1)
+    term.write(tab)
+end
 
-getfile("startup.lua","startup.lua")
+--/ Routine /--
